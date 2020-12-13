2181fc0c67d6f572a3a4370112fcabd0fcbe69bc
epsi sayidina <epsi.nurwijayadi@gmail.com>
2020-12-13 07:59:59 +0700

    Songs: Purescript using Spago

--- songs/purescript/Step07.purs
+++ songs/purescript/Step07.purs
@@ -1,15 +1,16 @@
 module Step07 where
 import Prelude
 import Effect.Console (log)
-import Data.Array (filter)
--- import Data.Array.Unique
+import Data.Array (filter, (:), cons, uncons)
+import Data.Maybe
 
 exclude :: String -> Array String -> Array String
 exclude tag xs = filter((/=) tag) xs
 
 unique :: Array String -> Array String
-unique [] = []
-unique (tag:tags) = cons tag unique(exclude tag tags)
+unique tags = case uncons tags of
+  Nothing -> []
+  Just {head: tag, tail: tags} -> cons tag (unique (exclude tag tags))
 
 tags :: Array String
 tags = ["rock", "jazz", "rock", "pop", "pop"]
