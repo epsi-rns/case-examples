708ec4930c8727b99d5c150f42af7ae8f1e6c8b2
epsi sayidina <epsi.nurwijayadi@gmail.com>
2020-12-27 23:15:57 +0700

    Songs: GNU R

--- songs/gnu-r/07-purrr.r
+++ songs/gnu-r/07-purrr.r
@@ -1,12 +1,10 @@
 # this is a nice to have part
 # you may skip this purrr part
 
-library(tibble)
+source("my-songs.r")
 library(purrr)
 
-songs_df <- read.csv2(file = "./songs.csv")
-
-songs_df$tags %>%
+dataframe$tags %>%
   walk(function(current) {
     current %>% as.list %>% paste %>% print
   })
