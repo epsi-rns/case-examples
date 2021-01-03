feba603809987b9bdac6225bd2afb7d9a74b2d5d
epsi sayidina <epsi.nurwijayadi@gmail.com>
2021-01-04 00:50:17 +0700

    Songs: C#

---
+++ songs/csharp/Step11Tree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+class Step11Tree
+{
+  public void Show() {
+    MySongs mySongs = new MySongs();
+  
+    var parameter  = Expression.Parameter(typeof(MySong), "MySong");
+    var type       = Type.GetType("MySong").GetProperty("Tags");
+    var property   = Expression.Property(parameter, type);
+    var constant   = Expression.Constant(null);
+    var comparison = Expression.NotEqual(property, constant);
+    var lambda     = Expression.Lambda<Func<MySong, bool>>(comparison, parameter);
+    Func<MySong, bool> withTagsFilterExpression = lambda.Compile();
+ 
+    var filteredSongs = (mySongs.songs)
+      .Where(withTagsFilterExpression);
+
+    foreach(var song in filteredSongs) {
+      Console.WriteLine("[{0}]", song);
+    }
+  }
+}
