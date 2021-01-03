f447bc20bc33b2319a86ef93090167a743e7f347
epsi sayidina <epsi.nurwijayadi@gmail.com>
2021-01-03 19:58:32 +0700

    Songs: C#

---
+++ songs/csharp/Step10Linq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Step10Linq
+{
+  public void Show() {
+     // 1. Data source.
+    var songs = new MySongs().songs;
+
+    // 2. Query creation.
+    var tagss =
+      from song in songs
+      where song.Tags != null
+        from tag in song.Tags
+        select tag;
+
+    // 3. Query execution.
+    var tags = tagss.Distinct();
+    Console.WriteLine("[{0}]",
+      string.Join(", ", tags));
+  }
+}
