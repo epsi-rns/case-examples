80313b1e267f5e29c920a824db06fd9cc142f938
epsi sayidina <epsi.nurwijayadi@gmail.com>
2021-01-10 18:57:20 +0700

    Songs: Rust: Neon Binding

--- songs/rust/neon-passarg/lib/index.js
+++ songs/rust/neon-passarg/lib/index.js
@@ -1,6 +1,17 @@
-const { passObject } = require('../native/index.node');
+const {
+  hello,
+  passStr,
+  passArray,
+  passObject
+} = require('../native/index.node');
+
+console.log(hello());
+
+console.log(passStr("Cantaloupe Island"));
+
+console.log(passArray(["60s", "jazz"]));
 
 console.log(passObject({
   title: "Cantaloupe Island",
   tags:  ["60s", "jazz"]
-}))
+}));
