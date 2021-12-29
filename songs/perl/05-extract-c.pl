98e9406f1a033919582bedc95f9efe4bec0a0352
Epsi Sayidina <epsi.nurwijayadi@gmail.com>
2021-12-30 00:42:48 +0700

    Songs: Perl

---
+++ songs/perl/05-extract-c.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use 5.010;
+use strict;
+use warnings;
+
+use File::Basename;
+use lib dirname(__FILE__);
+
+use MySongs;
+
+my @tagss = ();
+
+for (@MySongs::songs) { 
+  if (grep /tags/, keys %$_) {
+    push @tagss, $_->{'tags'};
+  }
+}
+
+for my $tags ( @tagss ) {
+  say join(":", @$tags);
+}
